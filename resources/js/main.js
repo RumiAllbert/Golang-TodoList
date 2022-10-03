5c2bfb29523fc7330da6361db3db897f705a1f12
Rumi Allbert <rumiallbert@gmail.com>
2022-10-03 11:57:29 -0400

    port update

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-var server = "https://hilarious-maamoul-dc3f3b.netlify.app:8000/";
+var server = "https://hilarious-maamoul-dc3f3b.netlify.app/";
 var todolist_server = server + "todo"
 
 function getCompletedTodos(){
